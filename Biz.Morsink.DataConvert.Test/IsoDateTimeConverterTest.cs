24f8313b58163d664e78747bc898b2a70558a3ec
Joost W. Morsink <morsink@gmail.com>
2017-03-16 20:37:04 +0100

    Added ISO DateTime converter

---
+++ Biz.Morsink.DataConvert.Test/IsoDateTimeConverterTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Biz.Morsink.DataConvert.Converters;
+
+namespace Biz.Morsink.DataConvert.Test
+{
+    [TestClass]
+    public class IsoDateTimeConverterTest
+    {
+        private DataConverter converter;
+
+        [TestInitialize]
+        public void Init()
+        {
+            converter = new DataConverter(new[] { new IsoDateTimeConverter() });
+        }
+        [TestMethod]
+        public void IsoDate_Happy()
+        {
+            Assert.AreEqual(new DateTime(2017, 3, 16, 19, 31, 0, DateTimeKind.Utc),
+                converter.Convert("2017-03-16T19:31:00Z").To<DateTime>());
+            Assert.AreEqual(new DateTimeOffset(2017, 3, 16, 20, 31, 0, TimeSpan.FromHours(1.0)),
+                converter.Convert("2017-03-16T20:31:00+01:00").To<DateTime>());
+            Assert.AreEqual("2017-03-16T19:31:00.000Z",
+                converter.Convert(new DateTime(2017, 3, 16, 19, 31, 0, DateTimeKind.Utc)).To<string>());
+            Assert.AreEqual("2017-03-16T20:31:00.000+01:00",
+                converter.Convert(new DateTimeOffset(2017, 3, 16, 20, 31, 0, TimeSpan.FromHours(1.0))).To<string>());
+
+        }
+    }
+}
---
+++ Biz.Morsink.DataConvert/Converters/IsoDateTimeConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Biz.Morsink.DataConvert.Converters
+{
+    public class IsoDateTimeConverter : IConverter
+    {
+        public static ConversionResult<string> ToIsoDate(DateTime datetime)
+            => new ConversionResult<string>(datetime.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ"));
+        public static ConversionResult<string> ToIsoDateOffset(DateTimeOffset datetime)
+            => new ConversionResult<string>(datetime.ToString("yyyy-MM-ddTHH:mm:ss.fffK"));
+
+        public static ConversionResult<DateTime> FromIsoDate(string str)
+        {
+            if (DateTime.TryParse(str, null, DateTimeStyles.RoundtripKind, out var res))
+                return new ConversionResult<DateTime>(res.ToUniversalTime());
+            else
+                return default(ConversionResult<DateTime>);
+        }
+        public static ConversionResult<DateTimeOffset> FromIsoDateOffset(string str)
+        {
+            if (DateTimeOffset.TryParse(str, null, DateTimeStyles.RoundtripKind, out var res))
+                return new ConversionResult<DateTimeOffset>(res);
+            else
+                return default(ConversionResult<DateTimeOffset>);
+        }
+
+        public bool CanConvert(Type from, Type to)
+            => from == typeof(string) && (to == typeof(DateTime) || to == typeof(DateTimeOffset))
+            || to == typeof(string) && (from == typeof(DateTime) || from == typeof(DateTimeOffset));
+
+        public Delegate Create(Type from, Type to)
+        {
+            if (from == typeof(string))
+            {
+                if (to == typeof(DateTime))
+                    return new Func<string, ConversionResult<DateTime>>(FromIsoDate);
+                else if (to == typeof(DateTimeOffset))
+                    return new Func<string, ConversionResult<DateTimeOffset>>(FromIsoDateOffset);
+            }
+            else if (to == typeof(string))
+            {
+                if (from == typeof(DateTime))
+                    return new Func<DateTime, ConversionResult<string>>(ToIsoDate);
+                else if (from == typeof(DateTimeOffset))
+                    return new Func<DateTimeOffset, ConversionResult<string>>(ToIsoDateOffset);
+            }
+            throw new ArgumentException("Cannot convert between types");
+        }
+    }
+}
