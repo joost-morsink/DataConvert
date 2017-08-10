5d45c65539c8a0379a8caef91ace4078f9582a5a
Joost W. Morsink <morsink@gmail.com>
2017-08-10 14:52:05 +0200

    Extra test for ToObjectConverter

--- Biz.Morsink.DataConvert.Test/ToObjectConverterTest.cs
+++ Biz.Morsink.DataConvert.Test/ToObjectConverterTest.cs
@@ -14,7 +14,7 @@ namespace Biz.Morsink.DataConvert.Test
         [TestInitialize]
         public void Init()
         {
-            converter = new DataConverter(ToObjectConverter.Instance);
+            converter = new DataConverter(ToObjectConverter.Instance, TupleToArrayConverter.Instance);
         }
 
         [TestMethod]
@@ -24,6 +24,21 @@ namespace Biz.Morsink.DataConvert.Test
             Assert.AreEqual(42, x);
             Assert.IsTrue(converter.Convert<string>(null).TryTo(out x));
             Assert.IsNull(x);
+        }
+        [TestMethod]
+        public void ToObj_Indirect()
+        {
+            Assert.IsTrue(converter.Convert((1, 2, 3)).TryTo(out object[] x));
+            Assert.AreEqual(3, x.Length);
+            Assert.AreEqual(1, x[0]);
+            Assert.AreEqual(2, x[1]);
+            Assert.AreEqual(3, x[2]);
+            Assert.IsTrue(converter.Convert((42, "hoi", new Dictionary<string, string>())).TryTo(out x));
+            Assert.AreEqual(3, x.Length);
+            Assert.AreEqual(42, x[0]);
+            Assert.AreEqual("hoi", x[1]);
+            Assert.IsInstanceOfType(x[2], typeof(Dictionary<string, string>));
+
         }
 
     }
